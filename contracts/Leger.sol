a3145cc15913a2a6c7ef45a41f45995bea951fd7
Joey Zhao <zhaoyi0113@gmail.com>
2021-12-02 10:35:59 +1100

    add fee

--- contracts/Leger.sol
+++ contracts/Leger.sol
@@ -3,19 +3,11 @@ pragma solidity >=0.7.4;
 pragma experimental ABIEncoderV2;
 
 contract Ledger {
-    struct TransferRequest {
-        string title;
-        uint256 amount;
-        string bsb;
-        string accountName;
-        string accountNumber;
-    }
-
     mapping(address => uint256) balances;
 
     address payable owner;
 
-    event Transfered(bool _success, address _from, address _to, uint256 amount);
+    event Transfered(address _from, address _to, uint256 amount, uint256 fee);
 
     constructor() payable {
         owner = payable(msg.sender);
@@ -25,17 +17,15 @@ contract Ledger {
     function sendCoin(address payable receiver)
         public payable
     {
-        require(msg.sender == owner);
         uint256 amount = msg.value;
-        if (balances[msg.sender] < amount) return;
+        require(balances[msg.sender] >= amount, "out of balance");
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         receiver.transfer(amount);
-        emit Transfered(true, msg.sender, receiver, amount);
+        emit Transfered(msg.sender, receiver, amount, 0);
     }
 
     function getBalance(address addr) public view returns (uint256) {
         return balances[addr];
     }
-
 }
